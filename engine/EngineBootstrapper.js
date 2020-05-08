0c9bfb020f27e93c1a0d6941cf301269b5d324f7
Alex Goldring <travnick@gmail.com>
2020-05-08 11:40:07 +0200

    Added a bootstrapper to monitor engine startup process

---
+++ engine/EngineBootstrapper.js
@@ -0,0 +1,58 @@
+import LabelView from "../view/common/LabelView.js";
+import { FrameRunner } from "./graphics/FrameRunner.js";
+
+/**
+ * Used for monitoring progress of the engine startup
+ */
+export class EngineBootstrapper {
+
+    constructor() {
+        /**
+         *
+         * @type {Node}
+         */
+        this.rootNode = null;
+    }
+
+
+    /**
+     *
+     * @param {Engine} engine
+     * @returns {Promise}
+     */
+    boot(engine) {
+
+        const v = new LabelView('Engine initialization');
+        v.css({
+            color: 'white'
+        });
+        v.link();
+
+        this.rootNode.appendChild(v.el);
+
+        const started = engine.start();
+
+        const vExecutionEngineState = new LabelView('');
+        const vAssetManagerState = new LabelView('');
+
+        v.addChild(vExecutionEngineState);
+        v.addChild(vAssetManagerState);
+
+        const frameRunner = new FrameRunner(() => {
+            vExecutionEngineState.updateText(`Executor: ${engine.executor.queueReady.length} ready, ${engine.executor.queueUnresolved.length} unresolved`);
+            vAssetManagerState.updateText(`Assets: ${engine.assetManager.assets.size} loaded, ${engine.assetManager.requestMap.size} pending`);
+        });
+
+        frameRunner.startup();
+
+
+        started.then(() => {
+            frameRunner.shutdown();
+
+            this.rootNode.removeChild(v.el);
+        });
+
+
+        return started;
+    }
+}
