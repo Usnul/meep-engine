9d1cdc28882e81d627b7f74d6d2d1e3e7d45004b
Alex Goldring <travnick@gmail.com>
2020-06-09 18:17:23 +0200

    Removed curvature from moisture filter

--- generation/example/filters/SampleGroundMoistureFilter.js
+++ generation/example/filters/SampleGroundMoistureFilter.js
@@ -4,48 +4,28 @@ import { CellFilterSimplexNoise } from "../../filtering/complex/CellFilterSimple
 import { CellFilterConstant } from "../../filtering/core/CellFilterConstant.js";
 import { CellFilterGaussianBlur } from "../../filtering/complex/CellFilterGaussianBlur.js";
 import { CellFilterOneMinus } from "../../filtering/math/CellFilterOneMinus.js";
-import { CellFilterSmoothStep } from "../../filtering/math/CellFilterSmoothStep.js";
-import { CellFilterAngleToNormal } from "../../filtering/complex/CellFilterAngleToNormal.js";
-import Vector3 from "../../../core/geom/Vector3.js";
 import { CellFilterStep } from "../../filtering/math/CellFilterStep.js";
 import { CellFilterReadGridLayer } from "../../filtering/CellFilterReadGridLayer.js";
 import { MirGridLayers } from "../grid/MirGridLayers.js";
 
 const fReadHeight = CellFilterReadGridLayer.from(MirGridLayers.Heights);
 
-export const SampleGroundMoistureFilter =  CellFilterAdd.from(
+export const SampleGroundMoistureFilter = CellFilterAdd.from(
     // add regional variation in relative moisture
     CellFilterMultiply.from(
         CellFilterSimplexNoise.from(100, 100),
-        CellFilterConstant.from(0.2)
+        CellFilterConstant.from(0.4)
     ),
 
-    CellFilterAdd.from(
-        //simulate water accumulation from precipitation
-        CellFilterMultiply.from(
-            CellFilterGaussianBlur.from(
-                CellFilterOneMinus.from(
-                    CellFilterSmoothStep.from(
-                        CellFilterConstant.from(0),
-                        CellFilterConstant.from(Math.PI / 2),
-                        CellFilterAngleToNormal.from(fReadHeight, Vector3.forward)
-                    )
-                ),
-                2,
-                2
+    //simulate water distribution from lakes/rivers/sees into the nearby ground
+    CellFilterMultiply.from(
+        CellFilterGaussianBlur.from(
+            CellFilterOneMinus.from(
+                CellFilterStep.from(CellFilterConstant.from(0), fReadHeight)
             ),
-            CellFilterConstant.from(0.3)
+            7,
+            7
         ),
-        //simulate water distribution from lakes/rivers/sees into the nearby ground
-        CellFilterMultiply.from(
-            CellFilterGaussianBlur.from(
-                CellFilterOneMinus.from(
-                    CellFilterStep.from(CellFilterConstant.from(0), fReadHeight)
-                ),
-                5,
-                5
-            ),
-            CellFilterConstant.from(0.5)
-        )
+        CellFilterConstant.from(0.6)
     )
 );
