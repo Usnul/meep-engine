aea5a01458271eae8609182c2b452e8139564acd
Alex Goldring <travnick@gmail.com>
2020-06-18 04:34:17 +0200

    Better approximation of exponential decay

--- engine/sound/ecs/emitter/attenuateSoundLogarithmic.js
+++ engine/sound/ecs/emitter/attenuateSoundLogarithmic.js
@@ -18,10 +18,11 @@ export function attenuateSoundLogarithmic(distance, min, max) {
 
     const x = inverseLerp(min, max, distance);
 
-    //approximating logarithmic curve with 4th degree polynomial curve, curve fitted from sound pressure table up to 10 meters
     const x2 = x * x;
     const x3 = x2 * x;
     const x4 = x3 * x;
+    const x5 = x4 * x;
 
-    return 0.998869 - 5.533849 * x + 14.0474 * x2 - 15.6441 * x3 + 6.132812 * x4;
+    //approximating logarithmic curve with 5th degree polynomial curve, curve fitted from sound pressure table up to 10 meters
+    return -11.699 * x5 + 35.57 * x4 - 41.628 * x3 + 23.617 * x2 - 6.8598 * x + 0.9974;
 }
