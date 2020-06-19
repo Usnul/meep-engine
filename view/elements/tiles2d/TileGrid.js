70c2b2c25097ab9b9d9b3c1b5d045dff0ca38edf
Alex Goldring <travnick@gmail.com>
2020-06-19 04:52:18 +0200

    Fixed a bug that would allow illegal army swaps

--- view/elements/tiles2d/TileGrid.js
+++ view/elements/tiles2d/TileGrid.js
@@ -13,6 +13,7 @@ import Signal from "../../../core/events/signal/Signal.js";
 
 import { DragAndDropContext } from '../../common/dnd/DragAndDropContext.js';
 import { computeTileGridMove } from "../../../engine/ui/tiles2d/computeTileGridMove.js";
+import { assert } from "../../../core/assert.js";
 
 const slotElementPrototype = dom('div').addClass('marker').el;
 
@@ -54,11 +55,51 @@ class TileGridView extends View {
          */
         this.model = model;
 
+        /**
+         *
+         * @type {DragAndDropContext}
+         * @private
+         */
+        this.__dragAndDropContext = dragAndDropContext;
+        /**
+         *
+         * @type {boolean}
+         * @private
+         */
+        this.__enableDragAndDrop = enableDragAndDrop;
+        /**
+         *
+         * @type {Vector2}
+         * @private
+         */
+        this.__tileSize = tileSize;
+        /**
+         *
+         * @type {Vector2}
+         * @private
+         */
+        this.__tileSpacing = tileSpacing;
+        /**
+         *
+         * @type {Function}
+         * @private
+         */
+        this.__hookTileAdded = hookTileAdded;
+        /**
+         *
+         * @type {boolean}
+         * @private
+         */
+        this.__captureEventTap = captureEventTap;
+
         const dRoot = dom("div").addClass("ui-tile-grid-view");
         this.el = dRoot.el;
 
         const dMarkers = dom('div').addClass('marker-container');
         const vMarkers = new View();
+
+        this.__markerCountainer = vMarkers;
+
         vMarkers.el = dMarkers.el;
 
         const dTiles = dom('div').addClass('tile-container');
@@ -70,221 +111,295 @@ class TileGridView extends View {
 
         this.on.tap = new Signal();
 
-        const self = this;
 
-        const slots = this.slots = [];
-
-
-        function generateSlots(width, height) {
-            function makeSlotView(x, y) {
-                const v = new SlotView();
-                v.size.copy(tileSize);
-
-                const discretePosition = new Vector2(x, y);
-
-                const pos = TileView.calculatePosition(tileSize, tileSpacing, discretePosition);
-                v.position.copy(pos);
-
-                function validateDrop(draggable) {
-
-                    const view = draggable.view;
-
-                    /**
-                     * @type {Rectangle}
-                     */
-                    const tile = view.model;
-
-                    //check if tile will fit in the desired position
-                    if (
-                        discretePosition.x < 0
-                        || tile.size.x + discretePosition.x > model.size.x
-                        || discretePosition.y < 0
-                        || tile.size.y + discretePosition.y > model.size.y
-                    ) {
-                        return false;
-                    }
-
-                    const domain = draggable.parent.domain;
-                    if (domain === self) {
-                        //all well
-                        return true;
-                    }
-
-
-                    if (view.lockDragContext === true) {
-                        //item is not allowed to move to another context
-                        return false;
-                    }
-
-                    /**
-                     *
-                     * @type {TileGrid}
-                     */
-                    const targetGrid = model;
-
-                    /**
-                     * @type {TileGrid}
-                     */
-                    const sourceGrid = domain.model;
-
-                    const overlappingTiles = [];
-
-                    const overlappingTileCount = model.getOverlappingTilesRaw(overlappingTiles, discretePosition.x, discretePosition.y, tile.size.x, tile.size.y);
-
-                    const tileArea = tile.computeArea();
-
-                    //construct move plan
-                    const moveProgram = computeTileGridMove(tile, discretePosition.x, discretePosition.y, sourceGrid, targetGrid);
-
-                    if (moveProgram === null) {
-                        //move is impossible
-                        return false;
-                    }
-
-                    if (overlappingTileCount === 0) {
-                        // slot is unoccupied, validation depends on capacity constraint
-                        return model.capacity.getValue() >= model.computeTotalTileArea() + tileArea;
-                    } else {
-                        // slot is occupied
-
-
-                        // compute total overlap area
-                        let overlappingTileArea = 0;
-
-
-                        for (let j = 0; j < overlappingTileCount; j++) {
-                            const rectangle = overlappingTiles[j];
-                            overlappingTileArea += rectangle.computeArea();
-                        }
-
-                        if (model.capacity.getValue() < model.computeTotalTileArea() + tileArea - overlappingTileArea) {
-                            //grid can't take this tile
-                            return false;
-                        }
-
-                        if (sourceGrid.capacity.getValue() < sourceGrid.computeTotalTileArea() - tileArea + overlappingTileArea) {
-                            //old grid can't fit swapped tiles
-                            return false;
-                        }
-
-                        for (let j = 0; j < overlappingTileCount; j++) {
-                            const overlappingTile = overlappingTiles[j];
-
-                            const tileView = findTileView(overlappingTile);
-
-                            if (tileView.lockDragContext) {
-                                //swap target is not allowed to move, prevent this swap
-                                return false;
-                            }
-                        }
-
-                        //it's a tile swap, capacity will not be affected after transaction
-                        return true;
-                    }
-                }
-
-                if (enableDragAndDrop) {
-
-                    /**
-                     *
-                     * @type {DropTarget}
-                     */
-                    const target = dragAndDropContext.addTarget(v, self, validateDrop);
-
-                    target.on.added.add(function (draggable, oldParent) {
-
-                        /**
-                         * @type {TileGrid}
-                         */
-                        const otherGrid = oldParent.domain.model;
-
-                        const tile = draggable.view.model;
-
-                        const move = computeTileGridMove(tile, discretePosition.x, discretePosition.y, otherGrid, model);
-
-                        if (move !== null) {
-                            move.execute();
-
-                            move.instructions.forEach(function (instruction) {
-                                const tile = instruction.tile;
-                                //re-introduce tiles to trigger various updates in visualization chain
-                                instruction.target.tiles.removeOneOf(tile);
-                                instruction.target.tiles.add(tile);
-                            });
-
-                        } else {
-                            //move is not possible
-                            console.error('move is not possible');
-                        }
-                    });
-                    v.dropTarget = target;
-                }
-                return v;
-            }
-
-            let i = 0;
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-
-                    const v = makeSlotView(x, y);
-                    dMarkers.append(v.el);
-                    slots[i++] = v;
-                }
-            }
-        }
-
-        function getSlot(x, y) {
-            return slots[model.size.x * y + x];
-        }
-
-        function addTile(tile) {
-            const tileView = new TileView({ model: tile, spacing: tileSpacing, size: tileSize });
-
-            if (typeof hookTileAdded === "function") {
-                hookTileAdded(tileView, tile);
-            }
-
-            self.addChild(tileView);
-
-            const tileViewEl = tileView.el;
-            //sign up drag and drop
-            if (enableDragAndDrop) {
-                const slot = getSlot(tile.position.x, tile.position.y);
-                const dropTarget = slot.dropTarget;
-                tileView.draggable = dragAndDropContext.addElement(tileView, dropTarget);
-            }
-            if (captureEventTap === true) {
-                tileViewEl.addEventListener('click', function (event) {
-                    self.on.tap.dispatch(tile, tileView, event);
-                });
-            }
-        }
-
-        function findTileView(tile) {
-            const children = self.children;
-            for (let i = 0; i < children.length; i++) {
-                const view = children[i];
-                if (view.model === tile) {
-                    return view;
-                }
-            }
-        }
-
-        function removeTile(tile) {
-            const view = findTileView(tile);
-            self.removeChild(view);
-        }
+        this.slots = [];
 
         //place markers
-        generateSlots(model.size.x, model.size.y);
+        this.generateSlots(model.size.x, model.size.y);
 
-        model.tiles.forEach(addTile);
-        model.tiles.on.added.add(addTile);
-        model.tiles.on.removed.add(removeTile);
+        model.tiles.forEach(this.addTile, this);
+        model.tiles.on.added.add(this.addTile, this);
+        model.tiles.on.removed.add(this.removeTile, this);
 
         const areaForTiles = TileView.calculateSize(tileSize, tileSpacing, model.size);
 
         this.size.copy(areaForTiles);
+    }
+
+    /**
+     *
+     * @param {Draggable} draggable
+     * @param {number} cell_x
+     * @param {number} cell_y
+     * @return {boolean}
+     * @private
+     */
+    __validateDrop(draggable, cell_x, cell_y) {
+        assert.defined(draggable, 'draggable');
+        assert.isNumber(cell_x, 'cell_x');
+        assert.isNumber(cell_y, 'cell_y');
+
+        /**
+         *
+         * @type {TileGrid}
+         */
+        const model = this.model;
+
+        const view = draggable.view;
+
+        /**
+         * @type {Rectangle}
+         */
+        const tile = view.model;
+
+        //check if tile will fit in the desired position
+        if (
+            cell_x < 0
+            || tile.size.x + cell_x > model.size.x
+            || cell_y < 0
+            || tile.size.y + cell_y > model.size.y
+        ) {
+            return false;
+        }
+
+        const domain = draggable.parent.domain;
+
+        if (domain === this) {
+            //all well
+            return true;
+        }
+
+
+        if (view.lockDragContext === true) {
+            //item is not allowed to move to another context
+            return false;
+        }
+
+        /**
+         *
+         * @type {TileGrid}
+         */
+        const targetGrid = model;
+
+        /**
+         * @type {TileGrid}
+         */
+        const sourceGrid = domain.model;
+
+        const overlappingTiles = [];
+
+        const overlappingTileCount = model.getOverlappingTilesRaw(overlappingTiles, cell_x, cell_y, tile.size.x, tile.size.y);
+
+        const tileArea = tile.computeArea();
+
+        //construct move plan
+        const moveProgram = computeTileGridMove(tile, cell_x, cell_y, sourceGrid, targetGrid);
+
+        if (moveProgram === null) {
+            //move is impossible
+            return false;
+        }
+
+        if (overlappingTileCount === 0) {
+            // slot is unoccupied, validation depends on capacity constraint
+            return model.capacity.getValue() >= model.computeTotalTileArea() + tileArea;
+        } else {
+            // slot is occupied
+
+            // compute total overlap area
+            let overlappingTileArea = 0;
+
+
+            for (let j = 0; j < overlappingTileCount; j++) {
+                const rectangle = overlappingTiles[j];
+                overlappingTileArea += rectangle.computeArea();
+            }
+
+            if (model.capacity.getValue() < model.computeTotalTileArea() + tileArea - overlappingTileArea) {
+                //grid can't take this tile
+                return false;
+            }
+
+            if (sourceGrid.capacity.getValue() < sourceGrid.computeTotalTileArea() - tileArea + overlappingTileArea) {
+                //old grid can't fit swapped tiles
+                return false;
+            }
+
+            const instructions = moveProgram.instructions;
+
+            const instructionCount = instructions.length;
+
+            for (let i = 0; i < instructionCount; i++) {
+                /**
+                 *
+                 * @type {TileMoveInstruction}
+                 */
+                const moveInstruction = instructions[i];
+
+                /**
+                 *
+                 * @type {Rectangle}
+                 */
+                const moveTile = moveInstruction.tile;
+
+                if (moveTile.lockDragContext === true && moveInstruction.source !== moveInstruction.target) {
+                    //attempting to move a tile across contexts, but the tile doesn't allow that
+                    return false;
+                }
+            }
+
+            //it's a tile swap, capacity will not be affected after transaction
+            return true;
+        }
+    }
+
+    __makeSlotView(x, y) {
+        const v = new SlotView();
+        v.size.copy(this.__tileSize);
+
+        const discretePosition = new Vector2(x, y);
+
+        const pos = TileView.calculatePosition(this.__tileSize, this.__tileSpacing, discretePosition);
+        v.position.copy(pos);
+
+        /**
+         *
+         * @type {TileGrid}
+         */
+        const model = this.model;
+
+        const self = this;
+
+        const validateDrop = (draggable) => {
+            return this.__validateDrop(draggable, discretePosition.x, discretePosition.y);
+        }
+
+        if (this.__enableDragAndDrop) {
+
+            /**
+             *
+             * @type {DropTarget}
+             */
+            const target = this.__dragAndDropContext.addTarget(v, self, validateDrop);
+
+            target.on.added.add(function (draggable, oldParent) {
+
+                /**
+                 * @type {TileGrid}
+                 */
+                const otherGrid = oldParent.domain.model;
+
+                const tile = draggable.view.model;
+
+                const move = computeTileGridMove(tile, discretePosition.x, discretePosition.y, otherGrid, model);
+
+                if (move !== null) {
+                    move.execute();
+
+                    move.instructions.forEach(function (instruction) {
+                        const tile = instruction.tile;
+                        //re-introduce tiles to trigger various updates in visualization chain
+                        instruction.target.tiles.removeOneOf(tile);
+                        instruction.target.tiles.add(tile);
+                    });
+
+                } else {
+                    //move is not possible
+                    console.error('move is not possible');
+                }
+            });
+            v.dropTarget = target;
+        }
+
+        return v;
+    }
+
+    /**
+     *
+     * @param {number} width
+     * @param {number} height
+     */
+    generateSlots(width, height) {
+
+        let i = 0;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+
+                const v = this.__makeSlotView(x, y);
+
+                this.__markerCountainer.addChild(v);
+
+                this.slots[i++] = v;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param {Rectangle} tile
+     */
+    addTile(tile) {
+        const tileView = new TileView({ model: tile, spacing: this.__tileSpacing, size: this.__tileSize });
+
+        if (typeof this.__hookTileAdded === "function") {
+            this.__hookTileAdded(tileView, tile);
+        }
+
+        this.addChild(tileView);
+
+        const tileViewEl = tileView.el;
+        //sign up drag and drop
+        if (this.__enableDragAndDrop) {
+            const slot = this.getSlot(tile.position.x, tile.position.y);
+            const dropTarget = slot.dropTarget;
+            tileView.draggable = this.__dragAndDropContext.addElement(tileView, dropTarget);
+        }
+
+        if (this.__captureEventTap === true) {
+            tileViewEl.addEventListener('click', (event) => {
+                this.on.tap.dispatch(tile, tileView, event);
+            });
+        }
+    }
+
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     * @return {View}
+     */
+    getSlot(x, y) {
+        /**
+         *
+         * @type {TileGrid}
+         */
+        const model = this.model;
+        const size = model.size;
+        return this.slots[size.x * y + x];
+    }
+
+    /**
+     *
+     * @param {Rectangle} tile
+     */
+    removeTile(tile) {
+        const view = this.findTileView(tile);
+        this.removeChild(view);
+    }
+
+    /**
+     *
+     * @param {Rectangle} tile
+     * @return {View}
+     */
+    findTileView(tile) {
+        const children = this.children;
+        for (let i = 0; i < children.length; i++) {
+            const view = children[i];
+            if (view.model === tile) {
+                return view;
+            }
+        }
     }
 
     getChildByUUID(uuid) {
