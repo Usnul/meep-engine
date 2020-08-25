8b07d165afa3100e5232a19898c70fbdce4ec0dc
Alex Goldring <travnick@gmail.com>
2020-08-25 20:40:40 +0200

    Added 3d background to the title screen

--- engine/scene/SerializedScene.js
+++ engine/scene/SerializedScene.js
@@ -5,6 +5,40 @@ import { emptyTask } from "../../core/process/task/TaskUtils.js";
 import { assert } from "../../core/assert.js";
 import { MirScene } from "../../../model/game/scenes/MirScene.js";
 
+/**
+ *
+ * @param {string} path
+ * @param {EntityComponentDataset} ecd
+ * @param {Engine} engine
+ * @returns {Promise}
+ */
+export function loadSerializedScene(path, ecd, engine) {
+    const assetManager = engine.assetManager;
+
+    return assetManager.promise(path, GameAssetType.ArrayBuffer)
+        .then(asset => {
+
+            /**
+             *
+             * @type {ArrayBuffer}
+             */
+            const buffer = asset.create();
+
+            const deSerializer = new BinaryBufferDeSerializer();
+            deSerializer.registry = engine.binarySerializationRegistry;
+
+            const binaryBuffer = new EncodingBinaryBuffer();
+
+            binaryBuffer.fromArrayBuffer(buffer);
+
+            const task = deSerializer.process(binaryBuffer, engine, ecd);
+
+            engine.executor.run(task);
+
+            return task.promise();
+        });
+}
+
 export class SerializedScene extends MirScene {
     /**
      *
@@ -29,32 +63,7 @@ export class SerializedScene extends MirScene {
         //expand dataset to make sure it can take all components
         ecd.setComponentTypeMap(engine.entityManager.getComponentTypeMap());
 
-        engine.assetManager.promise(this.path, GameAssetType.ArrayBuffer)
-            .then(asset => {
-
-                /**
-                 *
-                 * @type {ArrayBuffer}
-                 */
-                const buffer = asset.create();
-
-
-                const deSerializer = new BinaryBufferDeSerializer();
-                deSerializer.registry = engine.binarySerializationRegistry;
-
-                const binaryBuffer = new EncodingBinaryBuffer();
-
-                binaryBuffer.fromArrayBuffer(buffer);
-
-                const task = deSerializer.process(binaryBuffer, engine, ecd);
-
-                engine.executor.run(task);
-
-
-                task.on.completed.add(success);
-                task.on.failed.add(failure);
-
-            }, failure);
+        loadSerializedScene(this.path, ecd, engine);
 
         return emptyTask();
     }
